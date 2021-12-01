2b14b691eae410b0892c7d8c2a6da59d8f083218
caoruoyu <812253651@qq.com>
2021-12-01 21:44:02 +0800

    项目初始化

---
+++ mydo/src/test/java/com/bore/it/mydo/MydoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bore.it.mydo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
